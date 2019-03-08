57f9768491abc459e187dcdb00c02a13f2611b23
fusictokunaga <tokunaga@fusic.co.jp>
2019-03-08 13:35:28 +0900

    BaseControllerをCake\Controller\Controllerに変更

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -2,7 +2,7 @@
 
 namespace ContentsFile\Controller;
 
-use App\Controller\AppController as BaseController;
+use Cake\Controller\Controller as BaseController;
 use Cake\Controller\Component\AuthComponent;
 
 use Cake\Event\Event;
